5e286c8fae0aebd3c2f0c92f2504d06069fbcbde
Eshwar Ramesh <eshwar.ramesh@simplilearn.net>
2023-12-12 14:21:02 +0530

    Initial Commit

---
+++ Dayly/DaylyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaylyApp.swift
+//  Dayly
+//
+//  Created by Eshwar Ramesh on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DaylyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
